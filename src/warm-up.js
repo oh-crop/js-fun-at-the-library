60e592642818342fda606d5d367a3e6f6e96d991
Maddy Randle <maddyrandle318@gmail.com>
2023-06-13 22:39:10 -0500

    Add conditionals to log messages base on page length and book title

--- src/warm-up.js
+++ src/warm-up.js
@@ -50,14 +50,22 @@ isShortStory = pages.length < 25;
 // the statement 'I could read that in a day!', otherwise, log the statement 
 // 'Ok, maybe I could read that in a day and a half'
 
-
+if (pages.length < 1000) {
+    console.log('I could read that in a day!')
+} else {
+    console.log('Ok, maybe I could read that in a day and a half')
+}
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite book is [title]!'. Otherwise, log the
 // statement 'I recently read the [title] book'. In either case, the value stored in
 // the "title" variable should be included in the logged statement.
 
-
+if (isFavorite) {
+    console.log(`My favorit book is ${title}!`)
+} else {
+    console.log(`I recently read the ${title} book`)
+}
 
 //------------------------- Arrays -------------------------//
 
